4989454aac59571d85b47df9ae1f6a5a2f5110bc
Luan Gong <luan.gong@gmail.com>
2017-10-22 23:58:47 -0700

    Use multiple components
    The `AppComponent` is doing everything at the moment.  In the beginning,
    it showed details of a single hero.  Then it became a master/detail form
    with both a list of heroes and the hero detail.  Soon there will be new
    requirements and capabilities.  We can't keep piling features on top of
    features in one component; that's not maintainable.
    
    We'll need to break it up into sub-components, each focused on a
    specific task or workflow.  Eventually, the `AppComponent` could become
    a simple shell that hosts those sub-components.
    
    https://angular.io/tutorial/toh-pt3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { FormsModule    } from '@angular/forms';
 import { AppComponent   } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroDetailComponent,
       ],
       imports: [
         FormsModule
